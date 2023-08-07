fec246c06fb9c98a1f4c30357f49b8c1681c8adf
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-08-07 22:23:08 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -71,5 +71,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-
-console.log("shreyash What You want to become" );
