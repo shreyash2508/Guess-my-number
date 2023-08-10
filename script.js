08cd12eea72ce20d327a9cb61e5d4dd64e3d1315
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-08-10 16:39:22 +0530

    Initial COmmit

--- script.js
+++ script.js
@@ -71,3 +71,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+
